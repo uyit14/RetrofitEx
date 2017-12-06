259330e8bcc73eb4c698c4a8b11d4774fb09a147
uyit14 <14110230@student.hcmute.edu.vn>
2017-12-06 17:11:13 +0700

    swip refresh layout and progressdialog

--- app/src/main/java/com/example/uytai/retrofit/activity/MainActivity.java
+++ app/src/main/java/com/example/uytai/retrofit/activity/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.uytai.retrofit.activity;
 
+import android.app.ProgressDialog;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -24,7 +27,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -32,17 +35,23 @@ public class MainActivity extends AppCompatActivity {
     private final static String API_KEY = "7e8f60e325cd06e164799af1e317d7a7";
 
     Toolbar toolbar;
-    TextView tvConnect;
     CheckConnection cd;
+    private SwipeRefreshLayout refreshLayout;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressDialog = new ProgressDialog(this);
+
+        refreshLayout = findViewById(R.id.srlLayout);
+        refreshLayout.setOnRefreshListener(this);
+        refreshLayout.setColorSchemeResources(R.color.orange, R.color.colorGreyLight, R.color.colorPrimary);
+
 
         cd = new CheckConnection(this);
 
-        tvConnect = findViewById(R.id.tvcheckconnection);
         toolbar =  findViewById(R.id.toolbar);
         ActionToolbar();
 
@@ -52,21 +61,35 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
+        progressDialog.setTitle("Loading data...");
+        progressDialog.setMessage("Please wait while we loading data!");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+        SetAdapter();
+
+        //
+
+
+
+    }
+
+    private void SetAdapter() {
         //
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         //
         if(cd.isConnected()){
-        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+            ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
 
-        Call<MovieResponse> call = apiService.getTopRatedMovies(API_KEY);
+            Call<MovieResponse> call = apiService.getTopRatedMovies(API_KEY);
 
             call.enqueue(new Callback<MovieResponse>() {
                 @Override
                 public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
                     List<Movie> movies = response.body().getResults();
                     recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+                    progressDialog.dismiss();
                     Log.d(TAG, "Number of movies received:" + movies.size());
                 }
 
@@ -81,15 +104,21 @@ public class MainActivity extends AppCompatActivity {
             //tvConnect.setVisibility(View.VISIBLE);
             Toast.makeText(MainActivity.this, "Can't load data. Check your internet connection!!", Toast.LENGTH_LONG).show();
         }
-
-        //
-
-
-
     }
 
     private void ActionToolbar() {
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SetAdapter();
+           refreshLayout.setRefreshing(false);
+            }
+        }, 2500);
     }
 
 
